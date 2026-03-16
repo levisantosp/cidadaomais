65974f2e5f929d73bc255992f4af2ebaed6bb235
levisantosp <levi.pires2005@gmail.com>
2026-03-15 22:39:59 -0300

    chore: simplify schema definition in drizzle config

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
 	out: "./packages/db/src/migrations",
-	schema: [
-		"./packages/db/src/schemas/user.ts",
-		"./packages/db/src/schemas/session.ts",
-		"./packages/db/src/schemas/account.ts",
-		"./packages/db/src/schemas/verification.ts"
-	],
+	schema: "./packages/db/src/schemas/*.ts",
 	dbCredentials: {
 		url: Bun.DATABASE_URL
 	},
