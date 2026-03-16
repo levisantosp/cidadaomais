956ed28364b8dd863f1c8ad8dac288637487950b
levisantosp <levi.pires2005@gmail.com>
2026-03-16 09:40:45 -0300

    fix: correct database URL access in drizzle configuration

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
 	out: "./packages/db/src/migrations",
 	schema: "./packages/db/src/schemas/*.ts",
 	dbCredentials: {
-		url: Bun.DATABASE_URL
+		url: Bun.env.DATABASE_URL
 	},
 	dialect: "postgresql",
 	casing: "snake_case"
