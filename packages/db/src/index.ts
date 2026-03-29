4ae3da5a536143f54e410ce38a6076d5e2b24c51
levisantosp <levi.pires2005@gmail.com>
2026-03-29 14:42:10 -0300

    feat: configure drizzle ORM with schema and casing options

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,4 +1,10 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { env } from './env'
+import * as schema from './schemas'
 
-export const db = drizzle(env.DATABASE_URL)
+export const db = drizzle(env.DATABASE_URL, {
+  schema,
+  casing: 'snake_case'
+})
+
+export { schema }
