4e54bf8f051735a395700c639214796a25fd3312
levisantosp <levi.pires2005@gmail.com>
2026-03-29 15:19:29 -0300

    feat: enhance error handling with custom error responses

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,36 @@
 import { cors } from '@elysiajs/cors'
-import openapi from '@elysiajs/openapi'
+import { openapi } from '@elysiajs/openapi'
 import { Elysia } from 'elysia'
 import { logger } from 'logger'
 import { z } from 'zod'
 import { auth } from '@/auth'
 import { OpenAPI } from '@/plugins/auth-plugin'
+import { HttpException } from '@/utils/HttpException'
 
 const app = new Elysia()
+  .onError(ctx => {
+    if (ctx.code === 'VALIDATION') {
+      return ctx.status('Unprocessable Content', ctx.error)
+    }
+
+    if (ctx.error instanceof HttpException) {
+      return ctx.status(ctx.error.statusText, {
+        status: ctx.error.statusText,
+        code: ctx.code,
+        error: ctx.error.message
+      })
+    }
+
+    if (ctx.error instanceof Error) {
+      logger.error(ctx.error.stack ?? ctx.error)
+    }
+
+    return ctx.status('Internal Server Error', {
+      status: 'Internal Server Error',
+      code: ctx.code,
+      error: 'Internal Server Error'
+    })
+  })
   .get('/', () => ({ message: 'Hello, Elysia!' }))
   .use(
     openapi({
