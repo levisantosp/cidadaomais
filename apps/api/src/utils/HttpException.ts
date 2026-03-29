220d39d343223e3cbade718806573ffc8e729e2e
levisantosp <levi.pires2005@gmail.com>
2026-03-29 15:15:11 -0300

    feat: implement custom http exception classes for error handling

---
+++ apps/api/src/utils/HttpException.ts
@@ -0,0 +1,29 @@
+import type { StatusMap } from 'elysia'
+
+export class HttpException extends Error {
+  public readonly statusText: keyof StatusMap
+
+  public constructor(statusText: keyof StatusMap, message: string) {
+    super(message)
+    this.name = new.target.name
+    this.statusText = statusText
+  }
+}
+
+export class NotFoundException extends HttpException {
+  public constructor(message = 'Resource not found') {
+    super('Not Found', message)
+  }
+}
+
+export class UnauthorizedException extends HttpException {
+  public constructor(message = 'Unauthorized') {
+    super('Unauthorized', message)
+  }
+}
+
+export class ForbiddenException extends HttpException {
+  public constructor(message = 'Forbidden') {
+    super('Forbidden', message)
+  }
+}
