e7cdf765ea6af96512a5b36aaecc78388e662a85
luizinmonster <luizgustavoidepedente2@gmail.com>
2026-03-29 21:40:03 -0300

    feat: implement PUT route for editing entities with validation and error handling

--- apps/api/src/routes/put/edit-entity.ts
+++ apps/api/src/routes/put/edit-entity.ts
@@ -1,15 +1,39 @@
 import { Elysia } from 'elysia'
 import { z } from 'zod'
 import { authPlugin } from '@/plugins/auth-plugin'
+import { db, schema } from 'db'  
+import { eq } from 'drizzle-orm'
 
 export const editEntity = new Elysia()
   .use(authPlugin)
-  .get('/entities/:id', async (ctx) => {}, {
+ .put('/entities/:id', async ({ params, body, set }) => {
+      try {
+        const result = await db
+          .update(schema.entity)
+          .set({
+            name: body.name,
+          })
+          .where(eq(schema.entity.id, params.id))
+          .returning()
+
+        if (result.length === 0) {
+          set.status = 404
+          return { message: 'Entidade não encontrada.' }
+        }
+
+        return { message: 'Atualizado!', data: result }
+        // -----------------------------------------
+      } catch (error) {
+        console.error('Erro ao atualizar:', error)
+        set.status = 500
+        return { message: 'Erro interno ao salvar no banco.' }
+      }
+    }, {
     authorize: ['Administrator'],
     params: z.object({
       id: z.coerce.bigint()
     }),
     body: z.object({
-      name: z.string().min(2).optional()
+      name: z.string().min(2).optional() 
     })
   })
