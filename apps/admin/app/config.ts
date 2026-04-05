49ca83eb5636d09c6b8e24d169b2ac92aedf1596
levisantosp <levi.pires2005@gmail.com>
2026-04-05 17:11:51 -0300

    feat: add error messages for user authentication

---
+++ apps/admin/app/config.ts
@@ -0,0 +1,4 @@
+export const error: Record<string, string> = {
+  INVALID_EMAIL_OR_PASSWORD: 'Credenciais inválidas',
+  USER_ALREADY_EXISTS_USE_ANOTHER_EMAIL: 'Já existe uma conta com este e-mail. Use outro e-mail.'
+}
