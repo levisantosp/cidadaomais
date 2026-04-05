76fe566c40f3436f426ed793aed9ea2cd8dca2ad
levisantosp <levi.pires2005@gmail.com>
2026-04-05 18:38:47 -0300

    feat: add private layout

---
+++ apps/admin/app/layouts/private.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import AppSidebar from '~/components/app-sidebar.vue'
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '~/components/ui/sidebar'
+</script>
+
+<template>
+  <div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header class="flex h-14 items-center gap-2 border-b px-4">
+          <SidebarTrigger />
+        </header>
+        
+        <main><slot /></main>
+      </SidebarInset>
+    </SidebarProvider>
+  </div>
+</template>
