f41b59783b89f7ed39cc6212b61a9b42fd699bb0
levisantosp <levi.pires2005@gmail.com>
2026-04-05 17:14:46 -0300

    feat: add auth middleware

---
+++ apps/admin/app/middleware/auth.global.ts
@@ -0,0 +1,31 @@
+import { auth } from '~/lib/auth'
+
+const publicRoutes = [
+  {
+    path: '/login',
+    action: 'redirect'
+  },
+  {
+    path: '/registrar',
+    action: 'redirect'
+  }
+] as const
+
+export default defineNuxtRouteMiddleware(async (to) => {
+  if (import.meta.server) return
+
+  const publicRoute = publicRoutes.find((route) => route.path === to.path)
+  const session = await auth.getSession()
+
+  if (!session.data && publicRoute) return
+  if (!session.data && !publicRoute) {
+    return navigateTo('/login', {
+      replace: true
+    })
+  }
+  if (session.data && publicRoute?.action === 'redirect') {
+    return navigateTo('/', {
+      replace: true
+    })
+  }
+})
