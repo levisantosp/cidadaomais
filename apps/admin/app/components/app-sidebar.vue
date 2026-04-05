fb799a0761d050c7325bdaec4942f5c3e8eca763
levisantosp <levi.pires2005@gmail.com>
2026-04-05 18:38:58 -0300

    feat: add app sidebar component

---
+++ apps/admin/app/components/app-sidebar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Sidebar, SidebarHeader } from '~/components/ui/sidebar'
+
+</script>
+
+<template>
+  <Sidebar>
+    <SidebarHeader>
+      <h2>teste</h2>
+    </SidebarHeader>
+  </Sidebar>
+</template>
