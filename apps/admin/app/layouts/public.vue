efac580bf9896b2a129fd0bd9b23a794df9b75cf
levisantosp <levi.pires2005@gmail.com>
2026-04-04 22:13:42 -0300

    feat: add public layout

---
+++ apps/admin/app/layouts/public.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { Button } from '~/components/ui/button'
+</script>
+
+<template>
+  <div>
+    <header
+      class="flex justify-between px-4 py-5 sm:px-6 md:px-10 lg:px-16 xl:px-20"
+    >
+      <div class="cursor-pointer">
+        <h1 class="text-2xl font-[1000] md:text-4xl">
+          <span class="text-[#0186ca]">CIDADÃO</span>
+          <span class="text-[#3f9731]">MAIS</span>
+        </h1>
+      </div>
+
+      <div class="flex gap-2">
+        <NuxtLink to="/login">
+          <Button class="cursor-pointer rounded-sm">Entrar</Button>
+        </NuxtLink>
+        <NuxtLink to="/registrar">
+          <Button class="cursor-pointer rounded-sm" variant="secondary">
+            Criar conta
+          </Button>
+        </NuxtLink>
+      </div>
+    </header>
+    <main><slot /></main>
+  </div>
+</template>
