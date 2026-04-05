2da3dad1a7855c8b46bf90280a0daa934a9538c8
levisantosp <levi.pires2005@gmail.com>
2026-04-05 18:38:11 -0300

    feat: implement session handling with loading state in app component

--- apps/admin/app/app.vue
+++ apps/admin/app/app.vue
@@ -1,6 +1,23 @@
+<script setup lang="ts">
+import { Toaster } from '~/components/ui/sonner'
+import 'vue-sonner/style.css'
+import { auth } from '~/lib/auth'
+import Loading from '~/components/loading.vue'
+
+const session = auth.useSession()
+</script>
+
 <template>
-  <div>
-    <NuxtLayout />
-    <NuxtPage />
+  <div
+    v-if="session.isPending"
+    class="flex min-h-screen justify-center items-center"
+  >
+     <Loading />
+  </div>
+  <div v-else>
+    <div>
+      <NuxtLayout> <NuxtPage /> </NuxtLayout>
+    </div>
+    <Toaster position="top-center" theme="dark" />
   </div>
 </template>
