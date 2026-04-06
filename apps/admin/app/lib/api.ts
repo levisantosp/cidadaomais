62ac0cefb24f47fd208e137390ada6984777f660
levisantosp <levi.pires2005@gmail.com>
2026-04-05 22:56:20 -0300

    feat: update api configuration to include credentials in fetch options

--- apps/admin/app/lib/api.ts
+++ apps/admin/app/lib/api.ts
@@ -2,4 +2,8 @@ import { treaty } from "@elysiajs/eden"
 import type { App } from "api/app"
 import { env } from "~/env"
 
-export const api = treaty<App>(env.VITE_API_URL)
+export const api = treaty<App>(env.VITE_API_URL, {
+  fetch: {
+    credentials: "include"
+  }
+})
