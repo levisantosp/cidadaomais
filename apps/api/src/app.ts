747aab19d1505f582d00eefccc227fb31136c01f
levisantosp <levi.pires2005@gmail.com>
2026-04-05 21:45:39 -0300

    feat: implement main application logic in app.ts and refactor index.ts to use the new app module

---
+++ apps/api/src/app.ts
@@ -0,0 +1,65 @@
+import { cors } from "@elysiajs/cors"
+import { openapi } from "@elysiajs/openapi"
+import { Elysia } from "elysia"
+import { logger } from "logger"
+import { z } from "zod"
+import { auth } from "./auth"
+import { OpenAPI } from "./plugins/auth-plugin"
+import { deleteEntity } from "./routes/delete/delete-entity"
+import { getEntity } from "./routes/get/get-entity"
+import { createEntity } from "./routes/post/create-entity"
+import { editEntity } from "./routes/put/edit-entity"
+import { HttpException } from "./utils/HttpException"
+
+export const app = new Elysia()
+  .onError((ctx) => {
+    if (ctx.code === "VALIDATION") {
+      return ctx.status("Unprocessable Content", ctx.error)
+    }
+
+    if (ctx.error instanceof HttpException) {
+      return ctx.status(ctx.error.statusText, {
+        status: ctx.error.statusText,
+        code: ctx.code,
+        error: ctx.error.message
+      })
+    }
+
+    if (ctx.error instanceof Error) {
+      logger.error(ctx.error.stack ?? ctx.error)
+    }
+
+    return ctx.status("Internal Server Error", {
+      status: "Internal Server Error",
+      code: ctx.code,
+      error: "Internal Server Error"
+    })
+  })
+  .get("/", () => ({ message: "Hello, Elysia!" }))
+  .use(
+    openapi({
+      documentation: {
+        components: await OpenAPI.components,
+        paths: await OpenAPI.getPaths()
+      },
+      path: "/docs",
+      mapJsonSchema: {
+        zod: z.toJSONSchema
+      }
+    })
+  )
+  .use(
+    cors({
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      credentials: true,
+      allowedHeaders: ["Content-Type", "Authorization"]
+    })
+  )
+  .mount(auth.handler)
+  .use(deleteEntity)
+  .use(getEntity)
+  .use(createEntity)
+  .use(editEntity)
+
+export type App = typeof app
