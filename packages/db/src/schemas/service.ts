2dff905d45324844d7d3040f34d3f1884f303714
levisantosp <levi.pires2005@gmail.com>
2026-04-08 16:27:05 -0300

    feat: add category relationship to service schema and update service relations

--- packages/db/src/schemas/service.ts
+++ packages/db/src/schemas/service.ts
@@ -1,5 +1,7 @@
 import { bigint, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { snowflake } from "../utils/snowflake";
+import { category } from "./category";
+import { relations } from "drizzle-orm";
 
 export const service = pgTable("service", {
   id: bigint("id", {
@@ -7,13 +9,25 @@ export const service = pgTable("service", {
   })
     .primaryKey()
     .$defaultFn(snowflake),
+  categoryId: bigint("category_id", {
+    mode: "bigint"
+  })
+    .notNull()
+    .references(() => category.id, {
+      onDelete: "restrict"
+    }),
   name: text("name").notNull(),
   description: text("description").notNull(),
-  createdAt: timestamp("created_at")
-    .notNull()
-    .defaultNow(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at")
     .notNull()
     .defaultNow()
     .$onUpdateFn(() => new Date())
 });
+
+export const serviceRelations = relations(service, ({ one }) => ({
+  category: one(category, {
+    references: [category.id],
+    fields: [service.categoryId]
+  })
+}));
