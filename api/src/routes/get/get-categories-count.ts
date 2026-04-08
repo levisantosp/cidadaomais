af66b831fddfee3e2b3db17dfc6e51c02b5b2ed5
levisantosp <levi.pires2005@gmail.com>
2026-04-08 17:04:22 -0300

    fix: correct path

--- api/src/routes/get/get-categories-count.ts
+++ api/src/routes/get/get-categories-count.ts
@@ -4,7 +4,7 @@ import { Elysia } from "elysia";
 import { authPlugin } from "../../plugins/auth-plugin";
 
 export const getCategoriesCount = new Elysia().use(authPlugin).get(
-  "categories-count",
+  "/categories-count",
   async () => {
     const [rows] = await db
       .select({
