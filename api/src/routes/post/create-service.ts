4fe414e389b7ee063322e4725783ac8e3f7a9ddc
levisantosp <levi.pires2005@gmail.com>
2026-04-08 21:46:30 -0300

    feat: enhance createService endpoint to validate category existence before service creation

--- api/src/routes/post/create-service.ts
+++ api/src/routes/post/create-service.ts
@@ -1,15 +1,31 @@
 import { db, schema } from "db";
+import { eq } from "drizzle-orm";
 import { Elysia } from "elysia";
 import { z } from "zod";
 import { authPlugin } from "../../plugins/auth-plugin";
+import { NotFoundException } from "../../utils/HttpException";
 
 export const createService = new Elysia().use(authPlugin).post(
   "/services",
   async (ctx) => {
-    const [service] = await db
-      .insert(schema.service)
-      .values(ctx.body)
-      .returning();
+    const service = await db.transaction(async (tx) => {
+      const [category] = await tx
+        .select({
+          id: schema.category.id
+        })
+        .from(schema.category)
+        .where(eq(schema.category.id, ctx.body.categoryId));
+      if (!category) {
+        throw new NotFoundException("Category not found");
+      }
+
+      const [service] = await tx
+        .insert(schema.service)
+        .values(ctx.body)
+        .returning();
+
+      return service;
+    });
 
     return service;
   },
