8ffb127c7288ee51ef7455bb1fb544984eb5c456
levisantosp <levi.pires2005@gmail.com>
2026-04-08 23:04:21 -0300

    feat: implement paginated response utility for api data handling

---
+++ api/src/utils/paginated-response.ts
@@ -0,0 +1,13 @@
+export const paginatedResponse = <T>(
+  data: T[],
+  query: {
+    limit: number;
+    page: number;
+  }
+) => {
+  return {
+    page: query.page,
+    hasNextPage: data.length > query.limit,
+    data: data.slice(0, query.limit)
+  };
+};
