e6fd624bfbf49efb9b571b47450cb0c0d72fc29e
levisantosp <levi.pires2005@gmail.com>
2026-04-10 17:51:03 -0300

    feat: enhance createService endpoint to check for existing services and handle conflicts

--- api/src/routes/post/create-service.ts
+++ api/src/routes/post/create-service.ts
@@ -3,28 +3,40 @@ import { eq } from "drizzle-orm";
 import { Elysia } from "elysia";
 import { z } from "zod";
 import { authPlugin } from "../../plugins/auth-plugin";
-import { NotFoundException } from "../../utils/HttpException";
+import { ConflictException, NotFoundException } from "../../utils/HttpException";
 
 export const createService = new Elysia().use(authPlugin).post(
   "/services",
   async (ctx) => {
     const service = await db.transaction(async (tx) => {
-      const [category] = await tx
-        .select({
-          id: schema.category.id
-        })
-        .from(schema.category)
-        .where(eq(schema.category.id, ctx.body.categoryId));
+      const [[category], [service]] = await Promise.all([
+        tx
+          .select({
+            id: schema.category.id
+          })
+          .from(schema.category)
+          .where(eq(schema.category.id, ctx.body.categoryId)),
+        tx
+          .select({
+            id: schema.service.id
+          })
+          .from(schema.service)
+          .where(eq(schema.service.name, ctx.body.name))
+          .limit(1)
+      ]);
       if (!category) {
         throw new NotFoundException("Category not found");
       }
+      if (service) {
+        throw new ConflictException("A service with this name already exists");
+      }
 
-      const [service] = await tx
+      const [newService] = await tx
         .insert(schema.service)
         .values(ctx.body)
         .returning();
 
-      return service;
+      return newService;
     });
 
     return service;
