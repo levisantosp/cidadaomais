cbdf5fdd37e948f551495ad26e866e0df66ca9a1
levisantosp <levi.pires2005@gmail.com>
2026-04-10 16:18:53 -0300

    feat: enhance createCategory endpoint to prevent duplicate categories

--- api/src/routes/post/create-category.ts
+++ api/src/routes/post/create-category.ts
@@ -1,15 +1,32 @@
 import { db, schema } from "db";
+import { eq } from "drizzle-orm";
 import { Elysia } from "elysia";
 import { z } from "zod";
 import { authPlugin } from "../../plugins/auth-plugin";
+import { ConflictException } from "../../utils/HttpException";
 
 export const createCategory = new Elysia().use(authPlugin).post(
   "/categories",
   async (ctx) => {
-    const [category] = await db
-      .insert(schema.category)
-      .values(ctx.body)
-      .returning();
+    const category = await db.transaction(async (tx) => {
+      const [category] = await tx
+        .select({
+          id: schema.category.id
+        })
+        .from(schema.category)
+        .where(eq(schema.category.name, ctx.body.name))
+        .limit(1);
+      if (category) {
+        throw new ConflictException("A category with this name already exists");
+      }
+
+      const [newCategory] = await tx
+        .insert(schema.category)
+        .values(ctx.body)
+        .returning();
+
+      return newCategory;
+    });
 
     return category;
   },
