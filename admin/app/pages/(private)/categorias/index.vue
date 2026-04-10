8afa8268f913f462efaf8421bfff984184484189
levisantosp <levi.pires2005@gmail.com>
2026-04-10 17:20:35 -0300

    feat: add categories page

---
+++ admin/app/pages/(private)/categorias/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Plus } from "lucide-vue-next";
+import { Button } from "~/components/ui/button";
+import { Card, CardHeader, CardTitle } from "~/components/ui/card";
+
+definePageMeta({
+  layout: "private"
+});
+</script>
+
+<template>
+  <div class="space-y-6 p-6">
+    <div>
+      <h1 class="text-2xl font-bold">Gestão de Categorias</h1>
+      <p class="text-muted-foreground text-sm">
+        Veja e acompanhe as categorias cadastradas no sistema
+      </p>
+    </div>
+
+    <Card>
+      <CardHeader
+        class="flex flex-col gap-4 pb-2 md:flex-row md:items-center md:justify-between"
+      >
+        <CardTitle class="text-2xl">Lista de Categorias</CardTitle>
+
+        <NuxtLink href="/categorias/criar">
+          <Button class="cursor-pointer">
+            <Plus />
+            Adicionar uma nova categoria
+          </Button>
+        </NuxtLink>
+      </CardHeader>
+    </Card>
+  </div>
+</template>
