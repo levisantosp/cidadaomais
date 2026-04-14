a2b3313bbdb7d6ac90ea366d71340fd65abd0714
levisantosp <levi.pires2005@gmail.com>
2026-04-14 10:02:27 -0300

    feat: add wait function for asynchronous delays

--- admin/app/config.ts
+++ admin/app/config.ts
@@ -3,3 +3,7 @@ export const error: Record<string, string> = {
   USER_ALREADY_EXISTS_USE_ANOTHER_EMAIL:
     "Já existe uma conta com este e-mail. Use outro e-mail."
 };
+
+export const wait = async (secs: number) => {
+  await new Promise((r) => setTimeout(r, secs * 1000));
+};
