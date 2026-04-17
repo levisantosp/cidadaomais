612ce992338176a8b85f94467d3df44b5147e074
levisantosp <levi.pires2005@gmail.com>
2026-04-17 16:45:07 -0300

    fix: include entity

--- api/src/routes/get/get-units.ts
+++ api/src/routes/get/get-units.ts
@@ -7,17 +7,17 @@ import { paginatedResponse } from "../../utils/paginated-response";
 export const getUnits = new Elysia().get(
   "/units",
   async (ctx) => {
-    const units = await db
-      .select()
-      .from(schema.entityUnit)
-      .where(
-        ctx.query.name
-          ? ilike(schema.entityUnit.name, `%${ctx.query.name}%`)
-          : undefined
-      )
-      .orderBy(desc(schema.entityUnit.createdAt))
-      .offset((ctx.query.page - 1) * ctx.query.limit)
-      .limit(ctx.query.limit + 1);
+    const units = await db.query.entityUnit.findMany({
+      where: ctx.query.name
+        ? ilike(schema.entityUnit.name, `%${ctx.query.name}%`)
+        : undefined,
+      orderBy: desc(schema.entityUnit.createdAt),
+      offset: (ctx.query.page - 1) * ctx.query.limit,
+      limit: ctx.query.limit + 1,
+      with: {
+        entity: true
+      }
+    });
 
     return paginatedResponse(units, ctx.query);
   },
