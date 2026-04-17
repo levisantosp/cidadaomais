331a785fb6d248a33715a97dee09037dd250142d
levisantosp <levi.pires2005@gmail.com>
2026-04-17 17:22:42 -0300

    feat: enhance entity creation to update related entity units

--- api/src/routes/post/create-entity.ts
+++ api/src/routes/post/create-entity.ts
@@ -1,4 +1,5 @@
 import { db, schema } from "db";
+import { inArray } from "drizzle-orm";
 import { Elysia } from "elysia";
 import { z } from "zod";
 import { authPlugin } from "../../plugins/auth-plugin";
@@ -6,10 +7,22 @@ import { authPlugin } from "../../plugins/auth-plugin";
 export const createEntity = new Elysia().use(authPlugin).post(
   "/entities",
   async (ctx) => {
-    const [entity] = await db
-      .insert(schema.entity)
-      .values(ctx.body)
-      .returning();
+    const entity = await db.transaction(async (tx) => {
+      const [entity] = await tx
+        .insert(schema.entity)
+        .values(ctx.body)
+        .returning();
+
+      await tx
+        .update(schema.entityUnit)
+        .set({
+          entityId: entity.id
+        })
+        .where(inArray(schema.entityUnit.id, ctx.body.unitsIds));
+
+      return entity;
+    });
+
     return entity;
   },
   {
@@ -19,7 +32,8 @@ export const createEntity = new Elysia().use(authPlugin).post(
       description: z.string().min(2),
       phone: z.string().optional(),
       email: z.email().optional(),
-      website: z.url().optional()
+      website: z.url().optional(),
+      unitsIds: z.coerce.bigint().array().min(1).max(100)
     })
   }
 );
