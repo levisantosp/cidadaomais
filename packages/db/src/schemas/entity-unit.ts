36cbc0c0152145d67925bb53a8d7beb4af6b9c37
levisantosp <levi.pires2005@gmail.com>
2026-04-17 16:44:44 -0300

    feat: add relations for entity in entity_unit schema

--- packages/db/src/schemas/entity-unit.ts
+++ packages/db/src/schemas/entity-unit.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import {
   bigint,
   doublePrecision,
@@ -28,3 +29,10 @@ export const entityUnit = pgTable("entity_unit", {
     .defaultNow()
     .$onUpdateFn(() => new Date())
 });
+
+export const entityUnitRelations = relations(entityUnit, ({ one }) => ({
+  entity: one(entity, {
+    fields: [entityUnit.entityId],
+    references: [entity.id]
+  })
+}));
