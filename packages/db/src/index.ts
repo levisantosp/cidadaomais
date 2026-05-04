17a78a7b32d9a38ab8fae802cf50986b98dc4dfe
levisantosp <levi.pires2005@gmail.com>
2026-05-04 18:18:29 -0300

    feat: extend database types with Service and ServiceGetPayload

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,4 @@
+import type { BuildQueryResult, DBQueryConfig, ExtractTablesWithRelations } from 'drizzle-orm'
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { env } from './env'
 import * as schema from './schemas'
@@ -12,3 +13,12 @@ export { schema }
 export type Role = typeof schema.user.$inferSelect.role
 export type User = typeof schema.user.$inferSelect
 export type Category = typeof schema.category.$inferSelect
+export type Service = typeof schema.service.$inferSelect
+
+type Schema = ExtractTablesWithRelations<typeof schema>
+
+export type ServiceGetPayload<T extends DBQueryConfig<'many', true, Schema, Schema['service']>> = BuildQueryResult<
+  Schema,
+  Schema['service'],
+  T
+>
