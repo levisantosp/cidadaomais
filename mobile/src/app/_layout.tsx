22451283a3081220739898f1386be71aeaaa33db
levisantosp <levi.pires2005@gmail.com>
2026-05-04 17:35:44 -0300

    feat: add @tanstack/react-query into layout component

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,20 +1,25 @@
 import '../global.css'
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { PortalHost } from '@rn-primitives/portal'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import { useColorScheme } from 'react-native'
 import { AnimatedSplashOverlay } from '@/components/animated-icon'
 import { AppHeader } from '@/components/app-header'
 import AppTabs from '@/components/app-tabs'
 
+const queryClient = new QueryClient()
+
 export default function TabLayout() {
   const colorScheme = useColorScheme()
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppHeader />
-      <AppTabs />
-      <PortalHost />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <AnimatedSplashOverlay />
+        <AppHeader />
+        <AppTabs />
+        <PortalHost />
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
