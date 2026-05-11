f2b49eb074614a43edb3d69e4640f39bc00589b2
levisantosp <levi.pires2005@gmail.com>
2026-05-11 11:39:33 -0300

    feat: update app header styles

--- mobile/src/components/app-header.tsx
+++ mobile/src/components/app-header.tsx
@@ -4,12 +4,12 @@ import { Text } from '@/components/ui/text'
 
 export function AppHeader() {
   return (
-    <SafeAreaView edges={['top']} className='bg-background'>
+    <SafeAreaView edges={['bottom']}>
       <View className='px-5 pb-5'>
         <View className='flex flex-row items-center justify-start gap-1'>
           <Image source={require('@/assets/images/logo.png')} className='rounded-2xl h-14 w-14' />
 
-          <Text className='font-extrabold'>
+          <Text className='font-extrabold text-xl'>
             <Text className='text-[#0186ca]'>CIDADÃO</Text>
             <Text className='text-[#3f9731]'>MAIS</Text>
           </Text>
