2bf07447101dad0eada60275f5aa5b61524070c4
levisantosp <levi.pires2005@gmail.com>
2026-05-11 19:54:11 -0300

    fix: explicitly parse environment variables

--- mobile/src/env.ts
+++ mobile/src/env.ts
@@ -5,4 +5,7 @@ const schema = z.object({
   EXPO_PUBLIC_ENABLE_API_DELAY: z.stringbool().optional()
 })
 
-export const env = schema.parse(process.env)
+export const env = schema.parse({
+  EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL,
+  EXPO_PUBLIC_ENABLE_API_DELAY: process.env.EXPO_PUBLIC_ENABLE_API_DELAY
+})
