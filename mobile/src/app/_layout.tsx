541cbcdd98d5aae71089088fe421117464d4660a
levisantosp <levi.pires2005@gmail.com>
2026-05-11 11:40:33 -0300

    refactor: remove unused splash screen and header logic

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -5,22 +5,16 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { usePathname } from 'expo-router'
 import React from 'react'
 import { useColorScheme } from 'react-native'
-import { AnimatedSplashOverlay } from '@/components/animated-icon'
-import { AppHeader } from '@/components/app-header'
 import AppTabs from '@/components/app-tabs'
 
 const queryClient = new QueryClient()
 
 export default function TabLayout() {
   const colorScheme = useColorScheme()
-  const pathname = usePathname()
-  const showHeader = pathname !== '/about'
 
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <AnimatedSplashOverlay />
-        {showHeader && <AppHeader />}
         <AppTabs />
         <PortalHost />
       </ThemeProvider>
